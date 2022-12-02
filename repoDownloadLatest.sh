071d4a823d95d658815214151764e490cbaee189
Michael Heidelberger <mheide@desktop-mheidelberger.NVC>
2022-12-02 13:57:33 -0600

    Logseq Specific Commit

--- repoDownloadLatest.sh
+++ repoDownloadLatest.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#
-DOWNLOAD_URL=$(curl -s https://api.github.com/repos/logseq/logseq/releases/latest \
-    | grep url \
-    | grep AppImage \
-    | cut -d '"' -f 4)
-echo Download URL $DOWNLOAD_URL
-FILE_NAME=$(curl -s https://api.github.com/repos/logseq/logseq/releases/latest \
-    | grep url \
-    | grep AppImage \
-    | cut -d '/' -f 9)
 
-AFILE_NAME=${FILE_NAME::-1}
 
+#GITHUB_API=$(curl -s https://api.github.com/repos/logseq/logseq/releases/latest)
+#echo $GITHUB_API > api.json
+DOWNLOAD_URL=$(cat ./api.json | jq -r '.assets[] | select(.name|match("AppImage$")) | .browser_download_url')
+FILE_NAME=$(cat ./api.json | jq -r '.assets[] | select(.name|match("AppImage$")) | .name')
+DIR="/home/mheide/Programs"
+PROGRAM_NAME='Logseq.AppImage'
+echo $DOWNLOAD_URL
 echo $FILE_NAME
-echo $AFILE_NAME
 
+echo Downloading $FILE_NAME
 wget $DOWNLOAD_URL
+
+echo Moving $FILE_NAME to $DIR as $PROGRAM_NAME
+mv $FILE_NAME $DIR/$PROGRAM_NAME
+echo Making $PROGRAM_NAME executable
+chmod +x $DIR/$PROGRAM_NAME
+echo DONE!
+
